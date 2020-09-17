8d18022b6b03370c3ba6c2abfac411cea35fe7fb
Timon <tmbcyberman@gmail.com>
2020-09-17 14:19:45 -0600

    New mechanics

---
+++ Cyborg Shrimp/Assets/Scripts/NewPowerUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPowerUp : MonoBehaviour
+{
+    public int value;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        value++;
+        print(value);
+    }
+}
