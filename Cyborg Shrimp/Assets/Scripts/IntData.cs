d9f620dca0aebb7488529583e1648c5ef31008f5
Timon <tmbcyberman@gmail.com>
2020-10-14 07:55:39 -0600

    Trying not to drown

---
+++ Cyborg Shrimp/Assets/Scripts/IntData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value;
+    
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+}
