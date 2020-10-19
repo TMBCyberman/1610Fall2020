6d30befe2c15e4e86fbc473500defafa76e931e1
Timon <tmbcyberman@gmail.com>
2020-10-19 09:54:20 -0600

    Did the challenge

--- Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    
+    public GameObject projectilePrefab;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    public float xRange = 10;
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
