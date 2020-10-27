292c9c44071841ef4180e7a2d5a69272f093ae1a
Timon <tmbcyberman@gmail.com>
2020-10-27 15:59:27 -0600

    Scriptable Object with Game Variables

---
+++ Cyborg Shrimp/Assets/Scripts/Collections.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections : MonoBehaviour
+{
+    public string singleString;
+    public List<Collectible> collectionlist;
+
+    public void Start()
+    {
+        foreach (var item in collectionlist)
+        {
+            var newItem = new GameObject(item.name);
+            var sprite = newItem.AddComponent<SpriteRenderer>();
+            sp
+        }
+    }
+}
---
+++ Cyborg Shrimp/Assets/Scripts/ObjectVariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVariables : MonoBehaviour
+{
+    public bool godtierance;
+    public float boon = 0f;
+    public float grist = 0f;
+    public float pluck = 0f;
+    public float vial = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
