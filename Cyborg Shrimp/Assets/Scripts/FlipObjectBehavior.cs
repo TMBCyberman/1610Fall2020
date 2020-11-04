ac04859dac965e2785418aa5d837ba80fccce9c4
Timon <tmbcyberman@gmail.com>
2020-11-03 21:39:13 -0700

    More stuff for class

--- Cyborg Shrimp/Assets/Scripts/FlipObjectBehavior.cs
+++ Cyborg Shrimp/Assets/Scripts/FlipObjectBehavior.cs
@@ -6,10 +6,19 @@ using UnityEngine;
 public class FlipObjectBehavior : MonoBehaviour
 {
     public float rotateValue;
-    private Vector3 rotateVector;
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rotateValue = 180;
+        }
         
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rotateValue = 0;
+        }
+        
+        transform.rotation = Quaternion.Euler(0, rotateValue, 0);
     }
 }
---
+++ Cyborg Shrimp/Assets/Scripts/Handler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Handler : MonoBehaviour
+{
+    public GameAction actionObj;
+    public UnityEvent handlerEvent;
+
+    private void Start()
+    {
+        actionObj.action += Handle;
+    }
+
+    private void Handle()
+    {
+        handlerEvent.Invoke();
+    }
+}
