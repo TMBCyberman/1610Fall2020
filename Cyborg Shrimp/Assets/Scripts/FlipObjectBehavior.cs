c257a0e1b22afac758aca6380bf494466ae83b52
Timon <tmbcyberman@gmail.com>
2020-11-03 19:47:00 -0700

    Today's class

---
+++ Cyborg Shrimp/Assets/Scripts/FlipObjectBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipObjectBehavior : MonoBehaviour
+{
+    public float rotateValue;
+    private Vector3 rotateVector;
+
+    private void Update()
+    {
+        
+    }
+}
