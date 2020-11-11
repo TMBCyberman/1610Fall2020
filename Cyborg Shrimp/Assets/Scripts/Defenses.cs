780f0901e4662f18179f06331c8dbf3129e845e8
Timon <tmbcyberman@gmail.com>
2020-11-10 19:03:34 -0700

    Defenses

---
+++ Cyborg Shrimp/Assets/Scripts/Defenses.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Defenses : ScriptableObject
+{
+    public enum DefenseItems
+    {
+        Shield,
+        Potions,
+        ExtraHealth,
+        Buffs,
+        SuperShield
+    }
+
+    public DefenseItems currentDefense;
+
+    public void RunCurrentDefense()
+    {
+        switch (currentDefense)
+        {
+            case DefenseItems.Shield:
+                //function 
+                break;
+            case DefenseItems.Potions:
+                //function 
+                break;
+            case DefenseItems.ExtraHealth:
+                //function 
+                break;
+            case DefenseItems.Buffs:
+                //function 
+                break;
+            case DefenseItems.SuperShield:
+                //function 
+                break;
+        }
+    }
+}
