ffac084a9cc9313af2b68487740e33498ed3d6c7
Timon <tmbcyberman@gmail.com>
2020-11-12 20:53:26 -0700

    At least I got something to work

---
+++ Create with Code/SendMeshHelp/Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
